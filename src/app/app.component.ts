93a0b5935cd891a5ae10a920828212f270566779
MagaAtaev <magaataev2005@gmail.com>
2023-10-29 16:54:03 +0300

    added forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { StoreService } from './store.service';
 import { RestService } from './rest.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +12,28 @@ import { RestService } from './rest.service';
 export class AppComponent {
   title = 'test4';
 
-  constructor(public store: StoreService, private rest: RestService) {
+  formGroup1: FormGroup;
+
+  constructor(public store: StoreService, private rest: RestService, private formBuilder: FormBuilder) {
     this.store.test3.subscribe((res) => console.log(res, "comp"))
     this.rest.getRoute().subscribe((res) => console.log(res))
+
+    this.formGroup1 = this.formBuilder.group({
+      input1: ["Привет", Validators.required],
+      inputSelect: ["two"],
+      inputText: ["Text"]
+    })
+  }
+
+  onSubmit(post: any) {
+    console.log(post)
+  }
+
+  test() {
+    this.formGroup1.get("input1")?.setValue("Hello")
+    this.formGroup1.patchValue({inputText: "text1111"})
+    const res = this.formGroup1.getRawValue()
+    console.log(res,
+    this.formGroup1.get("input1")?.value)
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +22,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
