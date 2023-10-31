5005a5fa6d37fc3315f459b1f8774ec857874007
MagaAtaev <magaataev2005@gmail.com>
2023-10-31 17:17:14 +0300

    Типизация реактивных форм

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,27 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { StoreService } from './store.service';
 import { RestService } from './rest.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Form } from './utils';
+
+// interface EditForm {       // Результат выполнения тип-обертки Form из файла Utils 
+//   input1: FormControl<string | null>;
+//   inputSelect: FormControl<string | null>;
+//   inputText: FormControl<string | null>;
+//   secondForm: FormGroup<{inputText: FormControl<string | null>}>
+// }
+
+const test = "input1"
+export interface EditModel {
+  [test]: string | null;
+  inputSelect: string | null;
+  inputText: string | null;
+  secondForm: TestModel;
+}
+
+interface TestModel {
+  inputText: string | null;
+}
 
 @Component({
   selector: 'app-root',
@@ -12,17 +32,26 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AppComponent {
   title = 'test4';
 
-  formGroup1: FormGroup;
+  //formGroup1: FormGroup<EditForm>;
+  //formGroup1: Form<EditModel>
+  formGroup1 = new FormGroup<Form<EditModel>>({     // С помощью типа Form мы генерируем новый тип для FormGroup на основе интерфейса EditModel который в результате будет схож с типом FormEdit
+    input1: new FormControl("Привет", [Validators.required]),
+    inputSelect: new FormControl ("two"),
+    inputText: new FormControl ("Text"),
+    secondForm: new FormGroup<Form<TestModel>>({
+      inputText: new FormControl("Text2")
+    })
+  })
 
   constructor(public store: StoreService, private rest: RestService, private formBuilder: FormBuilder) {
     this.store.test3.subscribe((res) => console.log(res, "comp"))
     this.rest.getRoute().subscribe((res) => console.log(res))
 
-    this.formGroup1 = this.formBuilder.group({
-      input1: ["Привет", Validators.required],
-      inputSelect: ["two"],
-      inputText: ["Text"]
-    })
+    // this.formGroup1 = this.formBuilder.group({
+    //   input1: ["Привет", Validators.required],
+    //   inputSelect: ["two"],
+    //   inputText: ["Text"]
+    // })
   }
 
   onSubmit(post: any) {
@@ -31,9 +60,17 @@ export class AppComponent {
 
   test() {
     this.formGroup1.get("input1")?.setValue("Hello")
-    this.formGroup1.patchValue({inputText: "text1111"})
-    const res = this.formGroup1.getRawValue()
-    console.log(res,
-    this.formGroup1.get("input1")?.value)
+    this.formGroup1.patchValue({
+      inputText: "text1111",
+      input1: "111"
+    })
+    //this.formGroup1.controls.input1.patchValue("Hi")
+    const res: EditModel = this.formGroup1.getRawValue()
+    this.rest.putData(res)
+    console.log(
+      res,
+      this.formGroup1.get("input1")?.value,
+      this.formGroup1.controls.input1.value
+    )
   }
 }
--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { EditModel } from './app.component';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +11,10 @@ export class RestService {
 
   getRoute() {
     return this.http.get("https://navig.kandidatrp.ru/ors/v2/directions/driving-car?start=45.69231,%2043.316699&end=45.687429,%2043.328728")
+  }
+
+  putData(data: EditModel) {
+    console.log(data)
+    //return this.http.put(url, data)
   }
 }
